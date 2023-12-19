6891ddfa716e9a9488b7ddc1dad7a9c0a49cf177
Loris Giuliani <4loris4@gmail.com>
2023-12-19 21:47:08 +0100

    Fixed issue with select placeholder on iOS

--- src/components/ui/DropdownListTile.tsx
+++ src/components/ui/DropdownListTile.tsx
@@ -13,11 +13,19 @@ type Props<T extends string | number | undefined> = Omit<JSX.IntrinsicElements["
 };
 
 export default function DropdownListTile<T extends string | number | undefined>({ label, placeholder, value, onChange, options, className, ...props }: Props<T>) {
+  const handleChange: React.ChangeEventHandler<HTMLSelectElement> = (e) => {
+    const value = e.target.value as T | undefined | "";
+    if (value == undefined || value == "") return;
+    onChange(value);
+  };
+
   return (
     <div className={styles.dropdownListTile}>
       <span>{label}</span>
-      <select key={value} value={value} onChange={(e) => onChange(e.target.value as T)} className={classNames(value == undefined && styles.empty, className)} {...props}>
-        <option hidden>{placeholder}</option>
+      <select key={value} value={value} onChange={handleChange} className={classNames(value == undefined && styles.empty, className)} {...props}>
+        {value == undefined &&
+          <option hidden>{placeholder}</option>
+        }
         {options.map(({ label, value }) => <option key={value} value={value}>{label}</option>)}
       </select>
     </div>
